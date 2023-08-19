4d077485267b0427c578bbfaed1364c596a32037
unknown <vsisodiyadesign@gmail.com>
2023-08-19 16:43:58 +0530

    descriptivelinkfix

--- next.config.js
+++ next.config.js
@@ -7,3 +7,7 @@ const nextConfig = {
 };
 
 module.exports = nextConfig;
+
+const withVideos = require('next-videos')
+
+module.exports = withVideos()
--- pages/index.js
+++ pages/index.js
@@ -73,6 +73,7 @@ const Home = ({
             )}
           </div>
         </div>
+        
       </section>
 
       {/* Home main */}
